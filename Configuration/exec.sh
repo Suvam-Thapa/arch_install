d4a0ae45600748259cb376a3259c510783ba2c37
Suvam-Thapa <140675864+Suvam-Thapa@users.noreply.github.com>
2024-04-03 20:38:04 +0545

    Update exec.sh

--- Configuration/exec.sh
+++ Configuration/exec.sh
@@ -12,9 +12,21 @@ HostName ()
 {
     read -p "Please provide a name for the host : " h_name
 }
+disk () {
+device=$(awk '$2 == "/" { print $1; exit }' /etc/mtab)
+disk_name=$(echo $device | sed 's/[0-9]*$//')
+}
+
 UserName
+
 clear
+
 HostName
+
 clear
+
 TimeZone
+
 clear
+
+disk
