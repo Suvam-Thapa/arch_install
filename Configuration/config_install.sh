4103f2dff3d6e7ac08d0e0b059f049670711d461
Suvam-Thapa <140675864+Suvam-Thapa@users.noreply.github.com>
2025-07-23 18:12:10 +0545

    Update config_install.sh

--- Configuration/config_install.sh
+++ Configuration/config_install.sh
@@ -4,6 +4,16 @@ Conf_Dir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 source $Conf_Dir/exec.sh
 
+mkdir -p /tmp/kernels
+cd /tmp/kernels
+
+curl -LO https://archive.archlinux.org/packages/l/linux-zen/linux-zen-6.6.zen1-1-x86_64.pkg.tar.zst
+curl -LO https://archive.archlinux.org/packages/l/linux-zen-headers/linux-zen-headers-6.6.zen1-1-x86_64.pkg.tar.zst
+
+cd 
+
+sudo pacman -U /tmp/kernels/linux-zen-6.6.zen1-1-x86_64.pkg.tar.zst /tmp/kernels/linux-zen-headers-6.6.zen1-1-x86_64.pkg.tar.zst --noconfirm
+
 ln -sf /usr/share/zoneinfo/$t_zone /etc/localtime
 
 hwclock --systohc
@@ -41,5 +51,11 @@ systemctl enable NetworkManager
 
 sed -i 's/^#HookDir/HookDir/' /etc/pacman.conf
 sed -i 's/^#Color/Color/' /etc/pacman.conf
+
+sudo tee -a /etc/pacman.conf >/dev/null <<'EOF'
+
+# --- lock kernel to 6.6.zen1 ---
+IgnorePkg = linux-zen linux-zen-headers
+EOF
 
 exit
